16de1202b51091aff2009bee552c89a8026ed849
arovdev <arthur.reshetniak@gmail.com>
2024-10-26 14:19:17 -0300

    Initial Commit

---
+++ Sway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sway
+//
+//  Created by Artur Reshetniak on 2024-10-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
